2bb8a7dc5fbe7884b66f2a67021a97bb6d014039
Rohit <rohitjoshi9july@gmail.com>
2023-04-06 18:47:23 +0000

    added terraform configuration

---
+++ terraform/variable.tf
@@ -0,0 +1,31 @@
+locals {
+  data_lake_bucket = "fhvhv-data-lake"
+}
+
+variable "project" {
+  description = "Your GCP Project ID"
+  default = "data-engineering-rj"
+  type = string
+}
+
+variable "region" {
+  description = "Region for GCP resources. Choose as per your location: https://cloud.google.com/about/locations"
+  default = "us-west1"
+  type = string
+}
+
+variable "storage_class" {
+  description = "Storage class type for your bucket. Check official docs for more info."
+  default = "STANDARD"
+}
+
+variable "BQ_DATASET" {
+  description = "BigQuery Dataset that raw data (from GCS) will be written to"
+  type = string
+  default = "fhvhv_trips_data"
+}
+
+variable credentials {
+  type = string
+  default = "~/.google/credentials/google_credentials.json"
+}
